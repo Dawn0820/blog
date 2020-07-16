90685d46c0164e7c918ac5d8fae4ecdd4e34ba9a
Cheon Saebyeok <lazyson0820@gmail.com>
2020-07-16 21:23:04 +0900

    test

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `Dawn's Devlog`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  author: `[새벽(Saebyeok)]`,
+  introduction: `이것저것`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `Dawn0820`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
+    instagram: `Dawn.1993`,
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
